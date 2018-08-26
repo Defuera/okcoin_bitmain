df6207fdb831b10dbc1e8516264c758e69c730e9
defuera <vdenis@zvooq.com>
2018-08-26 18:34:25 +0200

    fix error on start server

--- lib/orders_presenter.dart
+++ lib/orders_presenter.dart
@@ -4,11 +4,10 @@ import 'package:bitmain/api/order.dart';
 import 'package:bitmain/api_service.dart';
 import 'package:bitmain/orders_page.dart';
 
-
 class OrdersPagePresenter {
-
   static const _PER_PAGE = 20;
-  static const _ORDERS_TO_DISPLAY = 20;
+  static const _ORDERS_TO_DISPLAY = 30;
+  static const _POLL_DELAY_SECONDS = 2;
 
   final OrdersPageView _view;
   final _service = ApiService();
@@ -22,34 +21,48 @@ class OrdersPagePresenter {
   }
 
   void loadOrders() async {
-    print("loadOrders ${_orders.length}");
     try {
       final orders = await _service.getOrders(_orders.length, _PER_PAGE);
       onDataLoaded(orders);
-      Future.delayed(Duration(seconds: 2), () =>loadOrders());
+      loadDataDelayed();
     } catch (error) {
-      Future.delayed(Duration(seconds: 2), () =>loadOrders());
       _view.showError();
+      loadDataDelayed();
     }
-
   }
 
+  void loadDataDelayed() => Future.delayed(Duration(seconds: _POLL_DELAY_SECONDS), () => loadOrders());
+
+
   onDataLoaded(List<Order> orders) {
-    print("onDataLoaded ${orders.length}");
     _orders.addAll(orders);
 
     final sellOrders = _orders.where((it) => it.type == "sell").toList();
     final buyOrders = _orders.where((it) => it.type == "buy").toList();
 
     //order desc
-    buyOrders.sort((a, b) => b.price.compareTo(a.price));
-    buyOrders.length = _ORDERS_TO_DISPLAY;
+    buyOrders.sort(nullSafeSort);
+    trimList(buyOrders, _ORDERS_TO_DISPLAY);
 
     //order asc
-    sellOrders.sort((a, b) => a.price.compareTo(b.price));
-    sellOrders.length = _ORDERS_TO_DISPLAY;
+    sellOrders.sort((a, b) => nullSafeSort(b, a));
+    trimList(sellOrders, _ORDERS_TO_DISPLAY);
 
     _view.showData(buyOrders: buyOrders, sellOrders: sellOrders);
+  }
+
+  void trimList(List<Order> list, int length) {
+    if (list.length > length) {
+      list.length = length;
+    }
+  }
+
+  int nullSafeSort(a, b) {
+    if (a == null || b == null) {
+      return 0;
+    } else {
+      return b.price - a.price;
+    }
   }
 
 }
